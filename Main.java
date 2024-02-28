3d7502494b4732f7e7b83fd270105a1d39240733
Luke Collins <luke23571113@gmail.com>
2024-02-28 08:57:58 -0800

    first commit

---
+++ Main.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Main 
+{
+    public static int binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            // Check if the target is present at mid
+            if (arr[mid] == target) {
+                return mid;
+            }
+            
+            // If target greater, ignore left half
+            if (arr[mid] < target) {
+                left = mid + 1;
+            }
+            // If target is smaller, ignore right half
+            else {
+                right = mid - 1;
+            }
+        }
+        
+        // If we reach here, then the element was not present
+        return -1;
+    }
+
+
+    public static int elementWiseAddition (int[] arr)
+    {
+        int sum = 0; 
+        for (int i : arr)
+        {
+            sum += i; 
+        }
+        return sum; 
+    }
+
+    public static void main (String[] args)
+    {
+        int[] arr = {2, 3, 4, 10, 40};
+        int target = 10;
+        int result = binarySearch(arr, target);
+        if (result == -1) {
+            System.out.println("Element not present");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+}
